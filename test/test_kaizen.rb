10752a15178372e249f833e23bce2b1f5f66aafc
Sean Nieuwoudt <sean@wixelhq.com>
2016-06-06 13:22:33 +0200

    Cleanups

--- test/test_kaizen.rb
+++ test/test_kaizen.rb
@@ -3,24 +3,7 @@ require 'kaizen-cli'
 
 class KaizenTest < Minitest::Test
   def setup
-    # test download
-    # create project project
-    
+    # @TODO: Test Download
+    # @TODO: Test Unzip
   end
-
-  # def test_registry_load
-  #   assert_equal 2, Plugg.registry.length
-  # end
-  #
-  # def test_instance
-  #   response = Plugg.send(:test_method)
-  #
-  #   assert_instance_of Array, response
-  # end
-  #
-  # def test_output
-  #   response = Plugg.send(:test_method)
-  #
-  #   assert_equal response.first[:plugin], "Demo Plugin"
-  # end
 end
